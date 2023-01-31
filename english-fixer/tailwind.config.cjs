861019843566e97f39ac2205ae22a87a50300df3
Federico Villace <federicovillace@gmail.com>
2023-01-31 16:40:19 -0300

    started english fixer
    added astro
    added tailwind
    added svelte

---
+++ english-fixer/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./pages/**/*.{html,js}',
+		'./components/**/*.{html,js}',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	  ],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
