bc4e501352b1a53f601a56ecff0eb96e042075bb
Federico Villace <federicovillace@gmail.com>
2023-02-08 02:32:13 -0300

    update ia

--- english-fixer/src/services/ai.js
+++ english-fixer/src/services/ai.js
@@ -1,70 +1,60 @@
-const COHERE_API_KEY = "DDDvR4315GURC6C33KnG5ZdBpT6xt0zisDDJC9wk"
-const COHERE_API_GENERATE_URL = "https://api.cohere.ai/generate"
-const COHERE_API_DETECT_LANGUAGE = "https://api.cohere.ai/detect-language"
+const COHERE_API_KEY = "DDDvR4315GURC6C33KnG5ZdBpT6xt0zisDDJC9wk";
+const COHERE_API_GENERATE_URL = "https://api.cohere.ai/generate";
+const COHERE_API_DETECT_LANGUAGE = "https://api.cohere.ai/detect-language";
 
-export const checkEnglish = async  (input) =>{
-    const data = {
-        texts: [input]
-      }
-    
-      const { results } = await fetch(COHERE_API_DETECT_LANGUAGE, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          Authorization: `BEARER ${COHERE_API_KEY}`,
-          "Content-Type": 'application/json',
-          "Cohere-Version": '2022-12-06'
-        },
-        body: JSON.stringify(data)
-      }).then(res => res.json())
-    
-      const [{ language_code }] = results
-      return language_code === 'en'
-    }
+export const checkEnglish = async (input) => {
+  const data = {
+    texts: [input],
+  };
 
+  const { results } = await fetch(COHERE_API_DETECT_LANGUAGE, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `BEARER ${COHERE_API_KEY}`,
+      "Content-Type": "application/json",
+      "Cohere-Version": "2022-12-06",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
+
+  const [{ language_code }] = results;
+  return language_code === "en";
+};
 
 export const fixMyEnglish = async (input) => {
-    const data = {
-        model: "command-xlarge",
-        prompt:`This is a spell checker generator. 
+  const data = {
+    model: "command-xlarge",
+    prompt: `This is a spell checker generator. 
     -- 
-    Incorrect sample: "Chekin if now its working de way it should"
-    Correct sample: "Checking if now it's working the way it should"
+    Incorrect sample: You have ten years old
+    Correct sample: I are ten years old
     -- 
-    Incorrect sample: "You have ten years old"
-    Correct sample: "I are ten years old"
-    -- 
-    Incorrect sample: "I don't can't know"
-    Correct sample: "I don't know" 
+    Incorrect sample: I don't can't know
+    Correct sample: I don't know
     --
-    Incorrect sample: "${input}"
-    Correct sample:`,
-        max_tokens: 300,
-        temperature: 0.3,
-        k: 0,
-        p: 0.75,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop_sequences: [],
-        return_likelihoods: "NONE",
-      }
-    const response = await fetch(COHERE_API_GENERATE_URL, {
-        method: 'POST',
-        headers: {
-            Authorization: `BEARER ${COHERE_API_KEY}`,
-            'Content-Type': 'application/json', 
-            'Cohere-Version':'2022-12-06'
-        },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
+    Incorrect sample: ${input}
+    Correct sample: `,
+    max_tokens: 300,
+    temperature: 0.3,
+    k: 0,
+    p: 0.75,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: ["--"],
+    return_likelihoods: "NONE",
+  };
+  const response = await fetch(COHERE_API_GENERATE_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `BEARER ${COHERE_API_KEY}`,
+      "Content-Type": "application/json",
+      "Cohere-Version": "2022-12-06",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
 
-    const { text } = response.generations[0]
-    const length = input.length + 3 
-    console.log(text.slice(0, length))
+  const { text } = response.generations[0];
 
-    return text
-    .slice(0, length)
-    .replace('--', '')
-    .trim()
-    
-} 
+  return text.trim();
+};
